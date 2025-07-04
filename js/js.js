8fbe0d92199348804c3068494325aae507dd1803
Lilian Tarek <157750503+Lilian-Tarek@users.noreply.github.com>
2025-07-04 19:26:37 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,9 @@
+let toggler = document.querySelector(".toggler");
+let nav = document.querySelector(".navbar");
+toggler.addEventListener("click", function ()
+{   nav.classList.toggle("show");
+    }
+
+
+)
+// console.log(toggler);
